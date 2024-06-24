d668217373ab43734ad674c38f832c6211285044
faridasad <asadlifaridd@gmail.com>
2024-06-24 11:18:05 +0400

    migrate to express

--- main.js
+++ main.js
@@ -1,58 +1,47 @@
-// ESM
-import Fastify from "fastify";
+import express from "express";
 import puppeteer from "puppeteer";
-import fastifyCors from "@fastify/cors";
-import dotenv from "dotenv"
-dotenv.config()
+import cors from "cors";
+import bodyParser from "body-parser";
 
-const fastify = Fastify({
-  logger: true,
-});
+const app = express();
 
-fastify.register(fastifyCors, {
-  origin: "*", // Allow requests from any origin
-});
+// Middleware
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" })); // To handle large HTML content
 
-fastify.get("/", (request, reply) => {
-  reply.send({ hello: "world" });
-});
-fastify.post("/html2pdf", async (request, reply) => {
+app.post("/html2pdf", async (req, res) => {
   try {
-    const { html } = request.body; // Extract HTML from the request body
+    const { html } = req.body;
 
     if (!html) {
-      reply.status(400).send({ error: "HTML content is required" });
-      return;
+      return res.status(400).send({ error: "HTML content is required" });
     }
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"], // Add these args to prevent sandbox issues in some environments
-      timeout: 60000, // Increase the launch timeout to 60 seconds
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      timeout: 60000,
     });
+
     const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: "networkidle0" });
 
-    // Set the content of the page to the received HTML
-    await page.setContent(html, { waitUntil: "networkidle0" }); // Wait until the network is idle
-
-    // Generate PDF from the content
     const pdfBuffer = await page.pdf({
       format: "A3",
       printBackground: true,
-      timeout: 10000, // Increase the PDF generation timeout to 60 seconds
+      timeout: 60000,
     });
 
     await browser.close();
 
-    // Send the PDF file as response
-    reply.type("application/pdf").send(pdfBuffer);
+    res.type("application/pdf").send(pdfBuffer);
   } catch (error) {
-    fastify.log.error(error);
-    reply.status(500).send({ error: "Failed to generate PDF" });
+    console.error(error);
+    res.status(500).send({ error: "Failed to generate PDF" });
   }
 });
 
-fastify.listen({ port: process.env.PORT || 3001 }, (err, address) => {
-  if (err) throw err;
-  // Server is now listening on ${address}
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
